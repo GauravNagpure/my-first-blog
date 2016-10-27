eb688c5f22cd1e80119005f1f96f4a93dad8890d
newOne <gpnagpure@gmail.com>
2016-10-27 10:34:19 +0000

    hello

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url
 from . import views
-urlpatterns = patterns('',
-(r'^$', direct_to_template, {'template' : 'a.html'}),
+
+    urlpatterns = [
+        patterns('',url(r'^$', direct_to_template, {'template' : 'a.html'}),
+]
