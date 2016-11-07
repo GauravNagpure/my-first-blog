e8cd9fa8d51934266248e33121188241d437f353
Gaurav <gpnagpure@gmail.com>
2016-11-07 09:30:12 +0530

    authentication

---
+++ blog/migrations/0003_auto_20161104_1015.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0002_post_image'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='post',
+            name='image',
+        ),
+        migrations.AddField(
+            model_name='post',
+            name='domain',
+            field=models.CharField(max_length=200, null=True, blank=True),
+        ),
+    ]
